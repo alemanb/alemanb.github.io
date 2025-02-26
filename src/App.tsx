ceaf84d20ad2c791b97a346b1310493c988d91e4
alemanb <lovesohmong@gmail.com>
2025-02-26 13:37:54 -0600

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import MainCard from "./MainCard";
 const App = () => {
   return (
     <main className="flex flex-row min-h-screen justify-center items-center">
-      <div className="h-screen w-full grid grid-rows-1 grid-cols-1 lg:grid-cols-3 sm:grid-cols-1">        
-        <div className="hidden lg:block col-span-2 bg-white-500"></div>
+      <div className="h-screen w-full grid grid-rows-1 grid-cols-1 lg:grid-cols-3 sm:grid-cols-1">       
         <div className="p-8 bg-red-900 text-white">
             <div className = "flex">
               <MainCard/>
             </div>
         </div>
+        <div className="hidden lg:block col-span-2 bg-white-500"></div>
       </div>
     </main>
   )
--- src/MainCard.tsx
+++ src/MainCard.tsx
@@ -6,27 +6,36 @@ import {
     CardHeader,
     CardTitle,
   }  from "./components/ui/card"
-
+import alemanb from "../public/alemanb.jpg"
+import { Avatar, AvatarImage, AvatarFallback } from "./components/ui/avatar"
+import { Separator } from "./components/ui/separator"
 
 const MainCard = () => {
     return (
         <Card>
         <CardHeader>
-            <CardTitle>
+            <CardTitle className="flex gap-4">
                 <h1 className="">Benjamin Aleman</h1>
+                <Avatar className="size-15">
+                    <AvatarImage src={alemanb} alt="@alemanb"/>
+                    <AvatarFallback></AvatarFallback>
+                </Avatar>
             </CardTitle>
             <CardDescription>
-                <h2 className="">Computer Science | Music Technology at Texas A&M University</h2>
-                <div className="grid grid-cols-4">
-                    <a href="">resume</a>
-                    <a href="https://www.github.com/alemanb">github</a>
-                    <a href="https://www.linkedin.com/notifications/?filter=all">linkedin</a>
+                <h2 className="mb-2">Computer Science | Music Technology at Texas A&M University</h2>
+                <div className = "flex h-5 items-center space-x-4">
+                    <a href="../public/Benjamin_Aleman_2025.pdf" className="text-lg font-semibold">resume</a>
+                    <Separator orientation="vertical"/>
+                    <a href="https://www.github.com/alemanb" className="text-lg font-semibold">github</a>
+                    <Separator orientation="vertical"/>
+                    <a href="https://www.linkedin.com/in/benjamin-aleman-977854229/" className="text-lg font-semibold">linkedin</a>                
                 </div>
             </CardDescription>
         </CardHeader>
-        <CardContent>
+        <CardContent className= "grid gap-4">
             <p>Howdy! I am currently a computer science student at Texas A&M University interested in
             software development and machine learning.</p>
+            <img src="../public/primaryTAM.png" alt="" className=""/>
         </CardContent>
         <CardFooter>
         </CardFooter>
