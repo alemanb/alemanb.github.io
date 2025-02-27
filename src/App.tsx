203a17abae79ae23424909668376fdc0cb832b76
benji <lovesohmong@gmail.com>
2025-02-27 11:30:41 -0600

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import MainCard from "./MainCard";
 const App = () => {
   return (
     <main className="flex flex-row min-h-screen justify-center items-center">
-      <div className="h-screen w-full grid grid-rows-1 grid-cols-1 lg:grid-cols-3 md:grid-cols-1">       
-        <div className="p-8 h-screen bg-red-900 min-w-120 text-white scroll-smooth tracking-tight">
+      <div className="bgwhite h-screen w-full grid grid-rows-1 grid-cols-1 lg:grid-cols-3 md:grid-cols-1">       
+        <div className="p-8 bg-red-900 min-h-screen min-w-120 text-white scroll-smooth tracking-tight">
             <div className = "flex">
               <MainCard/>
             </div>
         </div>
-        <div className="lg:block hidden col-span-2 bg-white-500 scroll-smooth tracking-tight">
+        <div className="lg:block hidden col-span-2 max-w-700  scroll-smooth tracking-tight">
           <div className="flex flex-row min-h-screen justify-center items-center">
             <div className="h-80 w-60">
               <h1 className="text-6xl">something will go here</h1>
--- src/MainCard.tsx
+++ src/MainCard.tsx
@@ -14,7 +14,7 @@ import { ScrollArea } from "./components/ui/scroll-area"
 
 const MainCard = () => {
     return (
-        <Card>
+        <Card className="">
         <CardHeader>
             <CardTitle className="flex gap-4">
                 <h1 className="">Benjamin Aleman</h1>
@@ -38,7 +38,7 @@ const MainCard = () => {
             <p>Howdy! I am currently a sophmore computer science student at Texas A&M University interested in software development and machine learning.</p>
             <br></br>
             <p className="text-sm text-muted-foreground">shh.. hey, scroll me</p>
-            <ScrollArea className="bg-red-200 h-70 rounded-md p-4">
+            <ScrollArea className="bg-red-200 h-43 rounded-md p-4">
                 <h2 className="border-b-2 border-red-400">projects</h2>
                 <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
                     <li><div className="font-semibold">This website!</div> | Typescript, React, Shadcn</li>
